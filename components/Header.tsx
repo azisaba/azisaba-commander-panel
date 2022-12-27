b4e4628511712912353370d3c8475c75c425911a
testusuke <silverbro007@icloud.com>
2022-12-27 21:22:52 +0900

    feat: add setting button

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,6 +3,8 @@ import styles from "../styles/Header.module.scss"
 import Link from "next/link";
 import {Button} from "@mui/material";
 import {signOut, useSession} from "next-auth/react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faGear} from "@fortawesome/free-solid-svg-icons";
 
 export function Header() {
     const {data: session} = useSession()
@@ -10,6 +12,12 @@ export function Header() {
     return (
         <header>
             <div className={styles.header}>
+                <Link
+                    href={"/admin"}
+                    className={styles.settings}
+                >
+                    <FontAwesomeIcon icon={faGear} />
+                </Link>
                 <Link href={"/"}>
                     <h1 className={styles.title}>
                         Azisaba Commander
