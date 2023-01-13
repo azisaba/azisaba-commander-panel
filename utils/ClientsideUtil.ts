1cf29c5a3dc4cad970e84a578e8a02c2928ce2e2
testusuke <silverbro007@icloud.com>
2023-01-13 10:51:37 +0900

    fix: ip v6 does not work

---
+++ utils/ClientsideUtil.ts
@@ -0,0 +1,8 @@
+import {NextApiRequest} from "next";
+import requestIp from "request-ip";
+
+export const getIP = (req: NextApiRequest): string | undefined => {
+    const cfIP = req.headers['cf-connecting-ip'] as string
+    if (cfIP) return cfIP
+    return requestIp.getClientIp(req) as string
+}
